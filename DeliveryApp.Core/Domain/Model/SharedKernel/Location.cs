a6b4d517ab2aeecf5aa99cb6398c3181ca99c548
.Net_M0n$ter_1 <.Net_M0n$ter_1@DESKTOP-GGPKLPS>
2025-08-31 12:48:08 +0300

    Fix constans problem.
    Fix initialization order:
    1) Constructors
    2) Properties
    3) Fields
    
    Refactoring CalculateCurierDistanceToTargetLocation method
    (replace CalculateCurierStepsByCoordinate method with Math.Abs)
    
    Delete CalculateCurierStepsByCoordinate method

--- DeliveryApp.Core/Domain/Model/SharedKernel/Location.cs
+++ DeliveryApp.Core/Domain/Model/SharedKernel/Location.cs
@@ -1,14 +1,11 @@
 ﻿using CSharpFunctionalExtensions;
 using Primitives;
+using System.Runtime.CompilerServices;
 
 namespace DeliveryApp.Core.Domain.Model.SharedKernel
 {
     public sealed class Location : ValueObject
     {
-        public int X { get; }
-
-        public int Y { get; }
-
         private Location() { }
 
         private Location(int x, int y)
@@ -17,15 +14,19 @@ namespace DeliveryApp.Core.Domain.Model.SharedKernel
             Y = y;
         }
 
+        public int X { get; }
+
+        public int Y { get; }
+
+        private const int MaxCoordinateValue = 10;
+        private const int MinCoordinateValue = 0;
+
         public static Result<Location, Error> CreateLocation(int x, int y)
         {
-            const int maxBorder = 10;
-            const int minBorder = 0;
-
-            if (x > maxBorder || x <= minBorder)
+            if (x > MaxCoordinateValue || x <= MinCoordinateValue)
                 return GeneralErrors.ValueIsInvalid(nameof(x));
 
-            if (y > maxBorder || y <= minBorder)
+            if (y > MaxCoordinateValue || y <= MinCoordinateValue)
                 return GeneralErrors.ValueIsInvalid(nameof(y));
 
             return new Location(x, y);
@@ -33,40 +34,22 @@ namespace DeliveryApp.Core.Domain.Model.SharedKernel
 
         public static Result<Location, Error> CreateRandomLocation()
         {
-            const int maxValueBorder = 11;
-            const int minValueBorder = 1;
-
             var random = new Random();
 
-            var randomX = random.Next(minValueBorder, maxValueBorder);
-            var randomY = random.Next(minValueBorder, maxValueBorder);
+            var randomX = random.Next(MinCoordinateValue + 1, MaxCoordinateValue + 1);
+            var randomY = random.Next(MinCoordinateValue + 1, MaxCoordinateValue + 1);
 
             return CreateLocation(randomX, randomY);
         }
 
         public Result<int, Error> CalculateCurierDistanceToTargetLocation(Location target)
         {
-            var stepsCountByCoordinateX = CalculateCurierStepsByCoordinate(X, target.X);
-            var stepsCountByCoordinateY = CalculateCurierStepsByCoordinate(Y, target.Y);
+            var stepsCountByCoordinateX = Math.Abs(X - target.X);
+            var stepsCountByCoordinateY = Math.Abs(Y - target.Y);
 
             var curierStepsCountByTargetLocation = stepsCountByCoordinateX + stepsCountByCoordinateY;
 
             return curierStepsCountByTargetLocation;
-        }
-
-        private int CalculateCurierStepsByCoordinate(int coordinateFrom, int coordinateTo)
-        {
-            var coordinateMoveResult = 0;
-
-            while (coordinateFrom != coordinateTo)
-            { 
-                if (coordinateTo > coordinateFrom) coordinateFrom++;
-                if (coordinateTo < coordinateFrom) coordinateFrom--;
-
-                coordinateMoveResult++;    
-            }
-
-            return coordinateMoveResult;
         }
 
         protected override IEnumerable<IComparable> GetEqualityComponents()
